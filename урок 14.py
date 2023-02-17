27e85792e90c508bdfb9dd694a1a4194fbdc3dad
zhenua82 <zzhmenka@yandex.ru>
2023-02-17 17:49:47 +0300

    added lesson 14

---
+++ урок 14.py
@@ -0,0 +1,12 @@
+#Урок No14. 
+
+def recursion(x):
+    if x < 0:
+        return
+    recursion(x - 1)
+    print(x)
+    if x == (len(my_list) - 1):
+        print('Конец списка')
+    
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] 
+recursion(len(my_list) - 1)
