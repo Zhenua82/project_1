f01a624808097e787cd75431e935a9c1d983f2c0
Zhenua82 <zzhmenka@yandex.ru>
2023-05-04 21:16:21 +0300

    aaaaa

--- django_project-main/NewsProject/NewsPoject/News/urls.py
+++ django_project-main/NewsProject/NewsPoject/News/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 from News.views import index
 
@@ -9,5 +8,3 @@ urlpatterns = [
     path('', index),
     path('homework/', include('homework.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
